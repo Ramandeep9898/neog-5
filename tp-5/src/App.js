1d12f5602b8d4b2e81bb55c3180aa18efee3ac97
Ramandeep <ramank6238299@gmail.com>
2021-09-25 20:31:10 +0530

    start3

--- tp-5/src/App.js
+++ tp-5/src/App.js
@@ -2,18 +2,34 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😂": "laughing",
+  "😎": "Face With Sunglasses",
+  "❤": "Love",
+  "😢": "Sed",
+  "🎁": "Gift",
+};
+
 function App() {
-  const [userInput, setUserInput] = useState(" ");
+  const [meaning, setMeaning] = useState(" ");
 
   function onChangeHandler(event) {
-    setUserInput(event.target.value);
-  }
+    var userInput = event.target.value;
 
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = " We Don't have this in Our Database";
+    }
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
       <h1> Inside out!!</h1>
+
       <input onChange={onChangeHandler} />
-      <div>Welcome {userInput}</div>
+      <h2> Meaning: {meaning} </h2>
     </div>
   );
 }
